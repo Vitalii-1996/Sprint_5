1eeb03cba9e50e50d957b2e27aff5378c008c27f
Vitalii-1996 <vitalii.nikolaichuk1996@gmail.com>
2025-05-22 22:19:53 +0300

    Add test for create listing

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
-MAIN_URL = "https://qa-desk.stand.praktikum-services.ru/"
+MAIN_URL = 'https://qa-desk.stand.praktikum-services.ru/'
+CREATE_AD_URL = 'https://qa-desk.stand.praktikum-services.ru/create-lisiting'
+PROFILE_URL = "https://qa-desk.stand.praktikum-services.ru/profile"
 WRONG_EMAIL_MASK = 'WrongEmail@error'
 EXIST_USER_EMAIL = 'some_user@test.com'
 DEFAULT_PASSWORD = '12345678' 
 RED_BORDER_COLOR = 'rgb(255, 105, 114)'
+AUTH_REQUEST_TEXT = 'Чтобы разместить объявление, авторизуйтесь'
--- locators/locators.py
+++ locators/locators.py
@@ -21,6 +21,25 @@ class LoginLocators:
     LOGIN_BUTTON = By.XPATH, ".//form//button[contains(text(), 'Войти')]"
 
 class MainPageLocators:
-    PROFILE_IMAGE = By.XPATH, ".//div[contains(@class, 'header')]//button[@class = 'circleSmall']"
+    # PROFILE_IMAGE = By.XPATH, ".//div[contains(@class, 'header')]//button[@class = 'circleSmall']
+    PROFILE_IMAGE = By.CSS_SELECTOR, "button.circleSmall"
     PROFILE_NAME = By.XPATH, ".//div[contains(@class, 'header')]//h3[contains(@class, 'profileText')]"
     LOGOUT_BUTTON = By.XPATH, ".//div[contains(@class, 'header')]//button[contains(text(), 'Выйти')]"
+    CREATE_LISTING_BUTTON = By.XPATH, ".//div[contains(@class, 'header')]//button[contains(text(), 'Разместить объявление')]"
+    AUTH_REQUEST_TEXT = By.XPATH, ".//form[contains(@class, 'popUp')]//h1[contains(text(), 'Чтобы разместить объявление, авторизуйтесь')]"
+
+class CreateListingLocators:
+    LISTING_NAME = By.NAME, 'name'
+    LISTING_DESCRIPTION = By.XPATH, ".//div[contains(@class, 'textarea')]//textarea[@name = 'description']"
+    LISTING_PRICE = By.NAME, 'price'
+    
+    CATEGORY_DROPDOWN = By.XPATH, ".//button[preceding-sibling::input[@name = 'category']]"
+    CITY_DROPDOWN = By.XPATH, ".//button[preceding-sibling::input[@name = 'city']]"
+    DROPDOWN_ITEMS = By.XPATH, ".//div[contains(@class, 'dropDownMenu_options')]//button[contains(@class, 'dropDownMenu_btn')]"
+    RADIO_BUTTONS = By.XPATH, ".//input[@type = 'radio']/following-sibling::div[contains(@class, 'inputRegular')]"
+    SUBMIT_BUTTON = By.XPATH, ".//button[contains(text(), 'Опубликовать')]"
+
+class ProfileLocators:
+    NEXT_PAGE_BUTTON = By.XPATH, ".//button[contains(@class, 'arrowButton--right')]"
+    MY_ORDER_LAST_ITEM_NAME = By.XPATH, "//h1[text()='Мои объявления']/following-sibling::div//div[@class='card'][last()]//div[@class='about']//h2"
+    MY_ORDER_LAST_ITEM_PRICE = By.XPATH, "//h1[text()='Мои объявления']/following-sibling::div//div[@class='card'][last()]//div[@class='price']//h2"
